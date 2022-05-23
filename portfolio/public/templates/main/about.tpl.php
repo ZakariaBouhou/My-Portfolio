d7e1496ef6dce935838493865d5e3183e63219bd
ZakariaBouhou <zakbouhou1990@gmail.com>
2022-05-24 00:10:55 +0200

    template about changed

--- portfolio/public/templates/main/about.tpl.php
+++ portfolio/public/templates/main/about.tpl.php
@@ -1,9 +1,11 @@
 <div class="description" id="describe">
     <div class="content-description">
-        <h2 id="dev">Développeur junior PHP / Symfony</h2>
+        <h2 id="dev">Développeur Back-end</h2>
         <p>
-        Anciennement technicien Helpdesk en ESN et professeur de Parkour, je me suis formé dans le métier de développeur Web/Web Mobile dans un premier temps en autodicate puis au sein de l'école de formation <a href="https://oclock.io/"><strong>O'CLOCK.</strong></a>
+            Anciennement technicien Helpdesk en ESN et professeur de Parkour, je me suis formé dans le métier de développeur Web/Web Mobile dans un premier temps en       autodicate puis au sein de l'école de formation <a href="https://oclock.io/"><strong>O'CLOCK</strong></a>
         </p>
-        <p>Cette école m'a permis d'acquérir tous les outils de bases nécessaires au métier de développeur.</p>
+        <p>
+            Cette école m'a permis d'acquérir tous les outils de bases nécessaires au métier de développeur
+        </p>
     </div>
 </div>
