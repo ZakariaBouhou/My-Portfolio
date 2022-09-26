159db0457eac3a7260c60a2111e9a7a76c151be9
ZakariaBouhou <zakbouhou1990@gmail.com>
2022-09-26 19:42:42 +0200

    test commit

---
+++ portfolio/public/connexion.php
@@ -0,0 +1,25 @@
+<?php
+
+try {
+
+    $bdd = new PDO('mysql:host=zakboukdb.mysql.db;dbname=zakboukdb;charset=utf8', 'zakboukdb', 'Cuicui75012');
+
+}
+
+catch(Exception $e) {
+
+    die('Erreur : '.$e->getMessage());
+
+}
+
+/* try {
+
+    $bdd = new PDO('mysql:host=localhost;dbname=zakboukdb;charset=utf8', 'dbzak', 'Cuicui75012!');
+
+}
+
+catch(Exception $e) {
+
+    die('Erreur : '.$e->getMessage());
+
+} */
