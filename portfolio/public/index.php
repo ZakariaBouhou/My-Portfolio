7922a3eab71a1fc0f1d4a5e6c63f91d04baefcb3
ZakariaBouhou <zakbouhou1990@gmail.com>
2022-10-11 22:26:22 +0200

    fix home + starting projects

--- portfolio/public/index.php
+++ portfolio/public/index.php
@@ -6,8 +6,8 @@ require __DIR__ . '/vendor/autoload.php';
 //require_once __DIR__ . '/assets/options/connexion.php';
 require_once __DIR__ . '/templates/header.tpl.php';
 require_once __DIR__ . '/templates/main/home.tpl.php';
-require_once __DIR__ . '/templates/main/skills.tpl.php';
 require_once __DIR__ . '/templates/main/projects.tpl.php';
+require_once __DIR__ . '/templates/main/skills.tpl.php';
 require_once __DIR__ . '/templates/main/contact.tpl.php';
 require_once __DIR__ . '/templates/footer.tpl.php';
 
--- portfolio/public/templates/main/projects.tpl.php
+++ portfolio/public/templates/main/projects.tpl.php
@@ -1,3 +1,19 @@
-<div class="projects" id="projects">
-    
+<div class="all-projects">
+    <h2>Projets</h2>
+    <p>
+        Forum, site de petites annonces, clones, projet en groupe...<br>
+        Ici, mes principales réalisations présentées dans mes vidéos Youtube<br>
+        Ce qui est quand même plus sympathique qu'un repo Git !
+    </p>
+    <div class="projects-wrapper">
+        <div class="item-wrapper">
+            <a href="#" target="_blank"><img src="../../assets/images/projets/lebonRecoin.PNG"></a>
+        </div>
+        <div class="item-wrapper">
+            <a href="#" target="_blank"><img src="../../assets/images/projets/apex.PNG"></a>
+        </div>
+        <div class="item-wrapper">
+            <a href="#" target="_blank"><img src="../../assets/images/projets/caretheworld.PNG"></a>
+        </div>
+    </div>
 </div>
