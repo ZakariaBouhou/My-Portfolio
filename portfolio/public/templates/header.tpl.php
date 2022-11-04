90b07f8b4b30fabd61fbda8245f372fa5ee8120c
Zakaria BOUHOU <zakbouhou1990@gmail.com>
2022-11-04 17:20:10 +0100

    starting chore scss

--- portfolio/public/templates/header.tpl.php
+++ portfolio/public/templates/header.tpl.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio - Zakaria BOUHOU</title>
+    <title>Zakaria BOUHOU | Développeur PHP & Symfony</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link rel="icon" href="assets/zbfavicon.ico" />
     <link rel="stylesheet" href="assets/css/reset.css">
