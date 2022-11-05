768f27d9a4220869829a71c986b9579043a6257e
ZakariaBouhou <zakbouhou1990@gmail.com>
2022-11-05 17:31:08 +0100

    chore scss v1

--- portfolio/public/index.php
+++ portfolio/public/index.php
@@ -3,6 +3,7 @@
 //ini_set('display_errors',1);
 require __DIR__ . '/vendor/autoload.php';
 
+require_once __DIR__ . '/assets/options/connexion.php';
 require_once __DIR__ . '/templates/header.tpl.php';
 require_once __DIR__ . '/templates/main/home.tpl.php';
 require_once __DIR__ . '/templates/main/projects.tpl.php';
--- portfolio/public/templates/main/skills.tpl.php
+++ portfolio/public/templates/main/skills.tpl.php
@@ -1,12 +1,19 @@
+<?php
+    //$requete = $bdd->prepare('SELECT name, picture FROM technos');
+    //$requete->execute();
+?>
+
 <div class="abilities" id="abilities">
     <h2>Langages et outils</h2>
     <p>Mes compagnons de combat...</p>
     <div class="items">
-        <img src="../../assets/images/logo/php.png" alt="logo php">
-        <img src="../../assets/images/logo/symfony.png" alt="logo symfony">
-        <img src="../../assets/images/logo/mysql.png" alt="logo mysql">
-        <img src="../../assets/images/logo/js.png" alt="logo javascript">
-        <img src="../../assets/images/logo/github.png" alt="logo github">
-        <img src="../../assets/images/logo/vscode.png" alt="logo vs code">
-    </div>
+            <?php //while ($resultat = $requete->fetch()) { ?>          
+            <img src="../../assets/images/logo/php.png" alt="logo php">
+            <img src="../../assets/images/logo/symfony.png" alt="logo symfony">
+            <img src="../../assets/images/logo/mysql.png" alt="logo mysql">
+            <img src="../../assets/images/logo/js.png" alt="logo javascript">
+            <img src="../../assets/images/logo/github.png" alt="logo github">
+            <img src="../../assets/images/logo/vscode.png" alt="logo vs code">
+            <?php //} ?>
+        </div>
 </div>
