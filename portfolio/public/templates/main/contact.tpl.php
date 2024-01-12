f55bb197fef4e8e5f36e433506de2a1a5acc1bda
zakariabouhou <zakbouhou1990@gmail.com>
2024-01-12 21:23:38 +0100

    Logos added and clap animation ok

--- portfolio/public/templates/main/contact.tpl.php
+++ portfolio/public/templates/main/contact.tpl.php
@@ -4,9 +4,9 @@
         </h3>
         <li class="logo-contact">
             <a href="mailto:zak.bouhou@outlook.fr"><img src="../assets/images/logo/email.png" alt="Contact"></a>
-            <a href="https://linkedin.com/in/zakariabouhou75000" target="_blank"><img src="../assets/images/logo/linkedin-mini.png" alt="logo linkedin"></a>
+            <a href="https://linkedin.com/in/zakariabouhou75000" target="_blank"><img src="../assets/images/logo/linkedin32.png" alt="logo linkedin"></a>
             <a href="https://www.instagram.com/zpourzak/" target="_blank"><img src="../assets/images/logo/instagram.png" alt="logo linkedin"></a>
-            <a href="https://www.youtube.com/channel/UCh6u5pqLVadzCWTv8Xl2N5Q" target="_blank"><img src="../assets/images/logo/youtube-mini.png" alt="logo youtube"></a>
+            <a href="https://www.youtube.com/channel/UCh6u5pqLVadzCWTv8Xl2N5Q" target="_blank"><img src="../assets/images/logo/youtube32.png" alt="logo youtube"></a>
         </li> 
     </div>
 </main>
--- portfolio/public/templates/main/home.tpl.php
+++ portfolio/public/templates/main/home.tpl.php
@@ -1,9 +1,8 @@
 <main>
     <div class="home-wrapper" id="home">
         <div class="description anim-on">
-            <h1>Je réalise le montage de vos <span>vidéos</span>
-                <img src="../../assets/images/logo/clap64.png" alt="photo for desktop">
-            </h1>
+            <h1>Je réalise le montage de vos <span>vidéos</span></h1>
+            <img src="../../assets/images/logo/clap64.png" alt="photo for desktop" id="clap">
         </div>
     </div>
     
