1a561caeb9ee431d816bb6134865de4f770c0c67
zakariabouhou <zakbouhou1990@gmail.com>
2024-01-12 18:24:48 +0100

    suite

--- portfolio/public/templates/main/contact.tpl.php
+++ portfolio/public/templates/main/contact.tpl.php
@@ -1,14 +1,12 @@
     <div class="to-contact" id="to-contact">
-        <div class="left-side">
-            <h3>Me contacter</h3>
-            <a href="mailto:zak.bouhou@outlook.fr">Contact</a>
-        </div>
-        <div class="right-side">
-            <h3>Mes réseaux</h3>
-            <li class="logo-contact">
-                <a href="https://linkedin.com/in/zakariabouhou75000" target="_blank"><img src="../assets/images/logo/linkedin-mini.png" alt="logo linkedin"></a>
-                <a href="https://www.youtube.com/channel/UCh6u5pqLVadzCWTv8Xl2N5Q" target="_blank"><img src="../assets/images/logo/youtube-mini.png" alt="logo youtube"></a>
-            </li> 
-        </div>
+        <h3>Me contacter
+            <img src="../assets/images/logo/call-me.png" alt="Call me" id="call-me">
+        </h3>
+        <li class="logo-contact">
+            <a href="mailto:zak.bouhou@outlook.fr"><img src="../assets/images/logo/email.png" alt="Contact"></a>
+            <a href="https://linkedin.com/in/zakariabouhou75000" target="_blank"><img src="../assets/images/logo/linkedin-mini.png" alt="logo linkedin"></a>
+            <a href="https://www.instagram.com/zpourzak/" target="_blank"><img src="../assets/images/logo/instagram.png" alt="logo linkedin"></a>
+            <a href="https://www.youtube.com/channel/UCh6u5pqLVadzCWTv8Xl2N5Q" target="_blank"><img src="../assets/images/logo/youtube-mini.png" alt="logo youtube"></a>
+        </li> 
     </div>
 </main>
--- portfolio/public/templates/main/home.tpl.php
+++ portfolio/public/templates/main/home.tpl.php
@@ -1,10 +1,9 @@
 <main>
     <div class="home-wrapper" id="home">
-        <div class="description">
-            <h1>Je réalise le montage de vos vidéos</h1>
+        <div class="description anim-on">
+            <h1>Je réalise le montage de vos <span>vidéos</span>
+                <img src="../../assets/images/logo/clap64.png" alt="photo for desktop">
+            </h1>
         </div>
-        <picture>
-            <source srcset="../../assets/images/photos/photoSized2.png" alt="photo for mobile" media="(max-width: 767px)"> <img src="../../assets/images/photos/photo-desktop1.png" alt="photo for desktop">
-        </picture>
     </div>
     
